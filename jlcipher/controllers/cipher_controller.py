6e718d911250a8db4edd0b1aa660fe985ef288ec
Jimmy Lander <bonev1717@gmail.com>
2020-07-28 00:26:14 +0300

    Add cipher abstract base class in models. Also add Caesar and Vigenere derived classes. Clean up controller logic

--- jlcipher/controllers/cipher_controller.py
+++ jlcipher/controllers/cipher_controller.py
@@ -1,47 +1,19 @@
+from ..models.caesar_model import CaesarCipher
+from ..models.vigenere_model import VigenereCipher
+
+
 class CipherController:
-    def __init__(self, cipher_or_decipher, cipher_which, key):
-        self.cipher = cipher_or_decipher
-        self.cipher_which = cipher_which
-        self.key = key
+    def __init__(self):
+        self.cipher = None
 
-    def set_operation(self, cipher_or_decipher):
-        if cipher_or_decipher == 1:
-            self.cipher = True  # Cipher
-        else:
-            self.cipher = False  # Decipher
-
-    def set_cipher(self, cipher_which):
+    def load(self, cipher_which, text, key):
         if cipher_which == 'Caesar':
-            self.cipher_which = 'Caesar'
+            self.cipher = CaesarCipher(text, key)
         elif cipher_which == 'Vigenère':
-            self.cipher_which = 'Vigenère'
+            self.cipher = VigenereCipher(text, key)
 
-    def set_key(self, key):
-        self.key = key
-
-    def translate(self, text):
-        output = str()
-
-        if self.cipher_which == 'Caesar':
-            if self.cipher:
-                output = self.translate_caesar(text)
-            else:
-                output = self.translate_caesar(text, False)
-
-        elif self.cipher_which == 'Vigenère':
-            if self.cipher:
-                output = self.translate_vigenere(text)
-            else:
-                output = self.translate_vigenere(text, False)
-
-        return output
-
-    def translate_caesar(self, text, to_cipher=True):
-        print("IN FUNCTION: CAESAR")
-        print("RECEIVED:", self.cipher, self.cipher_which, self.key)
-        print(text)
-
-    def translate_vigenere(self, text, to_cipher=True):
-        print("IN FUNCTION: VIGENERE")
-        print("RECEIVED:", self.cipher, self.cipher_which, self.key)
-        print(text)
+    def translate(self, decipher=False):
+        if decipher:
+            return self.cipher.decipher()
+        else:
+            return self.cipher.cipher()
--- jlcipher/views/main_view.py
+++ jlcipher/views/main_view.py
@@ -1,17 +1,17 @@
 import wx
 from . import cipher_view
-from ..controllers import cipher_controller
+from ..controllers.cipher_controller import CipherController
 
 
 class MainApp(wx.App):
     def __init__(self, mode):
         self.mode = mode
-        self.cipher_controller = cipher_controller.CipherController(cipher_or_decipher=True, cipher_which='Caesar', key=3)
+        self.cipher_controller = CipherController()
 
         super().__init__()
 
     def OnInit(self):
-        self.frame = MainFrame(parent=None, title="JLCipher: The Quick Cipher App", pos=(20, 20), mode=self.mode)
+        self.frame = MainFrame(parent=None, title="JLCipher: The Quick Cipher App", pos=(20, 20), mode=self.mode)  # noqa: E501
         self.frame.Show()
 
         self.Bind(wx.EVT_BUTTON, self.translate, id=1)
@@ -19,28 +19,23 @@ class MainApp(wx.App):
         return True
 
     def translate(self, event):
-        operation = self.frame.cipher_panel.get_operation()
+        decipher = self.frame.cipher_panel.get_operation()  # boolean
         cipher_which = self.frame.cipher_panel.get_cipher()
         key = self.frame.cipher_panel.get_key()
-
         text = self.frame.cipher_panel.input_text.GetValue()
 
-        print("KEY: ", key)
-
-        self.cipher_controller.set_operation(operation)
-        self.cipher_controller.set_cipher(cipher_which)
-        self.cipher_controller.set_key(key)
-
-        self.cipher_controller.translate(text)
+        self.cipher_controller.load(cipher_which, text, key)
+        output = self.cipher_controller.translate(decipher)
+        self.frame.cipher_panel.output_text.SetValue(output)
 
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, title, pos, mode):
-        super().__init__(parent=parent, title=title, size=mode["WINDOW_SIZE"], pos=pos)
+        super().__init__(parent=parent, title=title, size=mode["WINDOW_SIZE"], pos=pos)  # noqa: E501
 
         self.SetMinSize(wx.Size(mode["MIN_WINDOW_SIZE"]))
 
-        self.cipher_panel = cipher_view.CipherPanel(parent=self, size=mode["WINDOW_SIZE"])
+        self.cipher_panel = cipher_view.CipherPanel(parent=self, size=mode["WINDOW_SIZE"])  # noqa: E501
         self.topbar_panel = cipher_view.BarPanel(parent=self)
         self.botbar_panel = cipher_view.BarPanel(parent=self)
 
